2e37d3c73435b54a1a5c198970388673af84fe7c
Rodel Newton <rodel100@gmail.com>
2020-01-07 06:42:50 -0500

    Second commit, FInished flash messaging feature

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
     def index
       @articles = Article.all
     end
@@ -6,7 +7,27 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])
     end
     def new
+      @article = Article.new
     end
-
-      
+    def create
+      @article = Article.new(article_params)
+      @article.save
+      flash.notice = "Article #{@article.title} created"
+      redirect_to article_path(@article)    
+    end    
+    def destroy
+      @article = Article.destroy(params[:id])
+      flash.notice = "Article #{@article.title} removed"
+      redirect_to articles_path
+    end
+    def edit
+      @article = Article.find(params[:id])
+    end
+    def update
+      @article = Article.find(params[:id])
+      @article.update(article_params)
+      flash.notice = "Article #{@article.title} updated"
+      redirect_to article_path(@article)
+    end
+    
 end
